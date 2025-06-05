cd2405fa0af20261f82bfba89e5a30084557844d
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-06-05 14:23:54 -0600

    Make a reusable TileList component render different item key values

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const TileList = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+export const TileList = ({ items }) => {
+  return items.map((item, index) => {
+    return (
+      <div key={index}>
+        <h1>{item.name || items.appointmentName}</h1>
+        {item.phoneNumber && <p>Phone: {item.phoneNumber}</p>}
+        {item.email && <p>Email: {item.email}</p>}
+        {item.date && <p>Date: {item.date}</p>}
+        {item.time && <p>Time: {item.time}</p>}
+        {item?.contact?.name && <p>Customer Name: {item.contact.name}</p>}
+      </div>
+    );
+  });
 };
