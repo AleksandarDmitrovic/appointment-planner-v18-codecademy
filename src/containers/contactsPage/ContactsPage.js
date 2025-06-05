6fbb9cf9776f00bca1bb5acc28ba424de5d090ea
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-06-05 12:12:27 -0600

    Add contact on handleSubmit func

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = ({ contacts }) => {
+export const ContactsPage = ({ contacts, addContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
@@ -19,6 +19,12 @@ export const ContactsPage = ({ contacts }) => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!isDuplicate) {
+      addContact(name, phoneNumber, email);
+      setName("");
+      setPhoneNumber("");
+      setEmail("");
+    }
   };
 
   /*
