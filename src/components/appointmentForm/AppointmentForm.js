aa1310050b56cc31d6c386b24fd165087de926ea
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-06-05 15:53:22 -0600

    Add form & inputs for AppointmentForm

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
+  const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 };
 
@@ -17,10 +15,33 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
-
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="date"
+        placeholder="Date"
+        min={getTodayString()}
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        required
+      />
+      <input
+        type="time"
+        placeholder="Time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        required
+      />
+      <button type="submit">Add Contact</button>
+    </form>
   );
 };
