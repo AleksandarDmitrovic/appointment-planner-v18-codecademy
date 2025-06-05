31a2bc3a2a6794a87f86d62817ba9febb3b8026f
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-06-05 15:41:52 -0600

    Create appointment data states + pass contacts to AppointmentPage cmpt

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -8,13 +8,16 @@ export const AppointmentsPage = () => {
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
   };
 
   return (
